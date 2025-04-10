0451363e9605a57f136bce0d7ae7cec09dc53b9b
PlazzaA <plazzagarciaalejandro@gmail.com>
2025-04-10 17:53:00 +0200

    Modeloupdate4

--- queries.sql
+++ queries.sql
@@ -1,3 +1,5 @@
+--Sacar una tabla con los alumnos como el ejemplo del csv
+
 SELECT 
     alumno.nombre,
     alumno.email,
@@ -8,7 +10,7 @@ SELECT
     aprobado.apto
 FROM rama
 INNER JOIN promocion
-ON rama.id_promocion = promocion.id_promocion
+ON rama.id_rama = promocion.id_rama
 INNER JOIN alumno
 ON promocion.id_promocion = alumno.id_promocion
 INNER JOIN proyecto
@@ -17,4 +19,26 @@ INNER JOIN aprobado
 ON aprobado.id_proyecto = proyecto.id_proyecto
 INNER JOIN campus
 ON prmocion.id_promocion = campus.id_promocion
-INNER JOIN 
+INNER JOIN tasistencia
+ON rama.id_rama = tasistencia.id_rama
+INNER JOIN modalidad
+ON rama.id_rama = modalidad.id_rama;
+
+--Sacar una tabla con los profesores como el ejemplo del csv
+
+SELECT 
+    profesor.nombre,
+    rol.nombre,
+    rama.nombre,
+    promocion.npromocion,
+    campus.nombre,
+    tasistencia.tipo
+FROM profesor
+INNER JOIN rama
+ON rama.id_rama = profesor.id_rama
+INNER JOIN promocion
+ON promocion.id_rama = rama.id_rama
+INNER JOIN rol
+ON rol.id_rol = profesor.id_rol
+INNER JOIN campus
+ON campus.id_promocion = promocion.id_promocion
